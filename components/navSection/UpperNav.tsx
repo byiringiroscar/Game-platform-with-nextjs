aeb82670411b1c434672d39420cb99aba9207c5f
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-19 14:24:42 +0200

    wip

--- components/navSection/UpperNav.tsx
+++ components/navSection/UpperNav.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
 
 const UpperNav = () => {
   return (
     <div className='w-full h-[30px] bg-slate-700'>
-        <div>
-            
+        <div className='flex gap-2 text-white items-center h-full'>
+            <FaFacebookF />
+            <FaTwitter />
+            <FaSquareInstagram />
+            <FaYoutube />
         </div>
     </div>
   )

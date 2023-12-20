38f353da45d21c967760cbd81a38874d3a850a7a
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-20 12:23:52 +0200

    wip Add dot

--- components/navSection/UpperNav.tsx
+++ components/navSection/UpperNav.tsx
@@ -3,10 +3,11 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaSquareInstagram } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa";
+import dot from './nav.module.css'
 
 const UpperNav = () => {
   return (
-    <div className='padding-container w-full py-2 bg-[#438F1B] flex justify-between'>
+    <div className={`padding-container w-full py-2 bg-[#438F1B] flex justify-between ${dot}`}>
         <div className='flex gap-3 text-white items-center h-full'>
             <FaFacebookF />
             <FaTwitter />
